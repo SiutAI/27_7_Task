281ee1d1483e444c6fcafb576d69ec328388d48e
Satius D <syutkinaai@gmail.com>
2022-08-24 22:49:30 +0300

    first commit

---
+++ src/main/java/StudyProfile.java
@@ -0,0 +1,13 @@
+public enum StudyProfile {
+    MEDICINE ("Медицина"),
+    BIOLOGY ("Биология"),
+    PHILOSOPHY ("Философия"),
+    MATH ("Математика");
+
+    private String profileName;
+
+    StudyProfile(String profileName) {
+        this.profileName = profileName;
+    }
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,17 @@
+public class main {
+    static Student student1 = new Student("Шаров Иван Петрович","ГУ", 2, 4);
+    static Student student2 = new Student("Тян Леонид Иванович", "ГУ", 3, 3);
+    static Student student3 = new Student("Шарова Наталья Петровна", "МУ", 5, 5);
+    static University university1 = new University("ГУ", "Главный Университет", "Гуга", 1953, StudyProfile.MATH);
+    static University university2 = new University("МУ", "Местный Университет", "Муга", 1923, StudyProfile.MEDICINE);
+
+    public static void main(String[] args) {
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        System.out.println(university1.toString());
+        System.out.println(university2.toString());
+    }
+
+
+}
